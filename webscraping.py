c3151094b7df70d40728308cec0e3cf318523d8d
F. Javier Morales M <fmoralesm87@gmail.com>
2023-04-26 00:24:31 -0500

    Elementos y clases

--- webscraping.py
+++ webscraping.py
@@ -11,14 +11,10 @@ url = 'https://super.walmart.com.mx/all-departments'
 header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 # Generamos la petición al sitio
-respuesta = requests.get(url, headers=header)
+response = requests.get(url, headers=headers)
 
 # Extraemos el contedio html del sitio
-soup = bs(respuesta.content, 'html.parser')
+soup = bs(response.text, 'html.parser')
 
-dep_box = soup.find_all('div', class_='h-100 relative')
-
-dep_box
-
-title = dep_box.find('h1').get_text()
-title
+# Elementos y clases
+divs = soup.find_all('div', {'class': 'flex flex-column'})
